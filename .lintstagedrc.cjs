6d6c372377b150dd27a88e74afdc7d33ab619c90
Sebastian Davids <sdavids@gmx.de>
2023-12-08 13:38:57 +0100

    chore: check files only do not reformat
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 // https://github.com/okonet/lint-staged#configuration
 
 module.exports = {
-  '*.{js,cjs,mjs,json}': ['eslint', 'prettier --write'],
-  '*.yaml': ['prettier --write'],
+  '*.{js,cjs,mjs,json}': ['eslint', 'prettier --check'],
+  '*.yaml': ['prettier --check'],
 };
