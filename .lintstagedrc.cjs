c6f7a170f51834e267afa96739d313b6330ed25c
Sebastian Davids <sdavids@gmx.de>
2024-02-16 17:51:25 +0100

    chore: add use strict to cjs files
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,7 @@
 // https://github.com/okonet/lint-staged#configuration
 
+'use strict';
+
 module.exports = {
   '*.{js,cjs,mjs,json}': ['eslint', 'prettier --check'],
   '*.yaml': ['prettier --check', 'yamllint --strict'],
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,3 +1,5 @@
+'use strict';
+
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
