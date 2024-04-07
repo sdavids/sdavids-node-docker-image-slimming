236b1559554ad687ecf1121f12ca61cf08697b04
Sebastian Davids <sdavids@gmx.de>
2024-04-07 15:11:29 +0200

    fix: remove extra log entries
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- src/js/healthcheck.mjs
+++ src/js/healthcheck.mjs
@@ -28,16 +28,12 @@ import process from 'node:process';
 
 const port = parseInt(process.env.PORT || '3000');
 if (isNaN(port) || port < 1 || port > 65535) {
-  const message = 'PORT needs to be a number in the range 1-65535';
-  console.error(message);
-  throw new RangeError(message);
+  throw new RangeError('PORT needs to be a number in the range 1-65535');
 }
 
 let protocol = process.env.PROTOCOL || 'http';
 if (!(protocol === 'http' || protocol === 'https')) {
-  const message = "PROTOCOL needs to be either 'http' or 'https'";
-  console.error(message);
-  throw new Error(message);
+  throw new Error("PROTOCOL needs to be either 'http' or 'https'");
 }
 protocol += ':';
 
@@ -45,10 +41,9 @@ const path = process.env.HEALTHCHECK_PATH || '/-/health/liveness';
 
 const timeout = parseInt(process.env.HEALTHCHECK_TIMEOUT_MS || '2000');
 if (isNaN(timeout) || timeout < 0) {
-  const message =
-    'HEALTHCHECK_TIMEOUT_MS needs to be a number greater or equal to 0 (timeout disabled)';
-  console.error(message);
-  throw new RangeError(message);
+  throw new RangeError(
+    'HEALTHCHECK_TIMEOUT_MS needs to be a number greater or equal to 0 (timeout disabled)',
+  );
 }
 
 // https://nodejs.org/api/http.html#http_http_request_options_callback
