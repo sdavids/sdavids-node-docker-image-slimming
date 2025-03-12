60f2850438c302af47ed74adb4667b18036b2c0e
Sebastian Davids <sdavids@gmx.de>
2025-03-12 16:16:26 +0100

    refactor: improve server
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- src/js/server.mjs
+++ src/js/server.mjs
@@ -1,11 +1,12 @@
 // SPDX-FileCopyrightText: © 2020 Sebastian Davids <sdavids@gmx.de>
 // SPDX-License-Identifier: Apache-2.0
 
-import express from 'express';
-import { faker } from '@faker-js/faker';
-import fs from 'node:fs';
+import { existsSync, readFileSync } from 'node:fs';
 import http from 'node:http';
 import https from 'node:https';
+import process from 'node:process';
+import express from 'express';
+import { faker } from '@faker-js/faker';
 
 ['uncaughtException', 'unhandledRejection'].forEach((signal) =>
   process.on(signal, (err) => {
@@ -17,13 +18,20 @@ import https from 'node:https';
   process.on(signal, () => process.exit(0)),
 );
 
-const port = process.env.PORT || 3000;
-const keyPath = process.env.KEY_PATH;
-const certPath = process.env.CERT_PATH;
+// eslint-disable-next-line dot-notation
+const port = Number(process.env['PORT'] ?? 3000);
+if (isNaN(port) || port < 1 || port > 65535) {
+  console.error(`port must be between 1 and 65535: ${port}`);
+  process.exit(64); // EX_USAGE
+}
+// eslint-disable-next-line dot-notation
+const certPath = process.env['CERT_PATH'];
+// eslint-disable-next-line dot-notation
+const keyPath = process.env['KEY_PATH'];
 
 let secure = keyPath && certPath;
 if (secure) {
-  secure = fs.existsSync(certPath) && fs.existsSync(keyPath);
+  secure = existsSync(certPath) && existsSync(keyPath);
 }
 
 const app = express();
@@ -33,7 +41,7 @@ app.set('port', port);
 app.disable('x-powered-by');
 
 app.get('/', (_, res) =>
-  res.set('Content-Type', 'application/json').send({
+  res.json({
     userId: faker.string.uuid(),
     username: faker.internet.userName(),
     email: faker.internet.email(),
@@ -42,24 +50,40 @@ app.get('/', (_, res) =>
 
 app.get('/-/health/liveness', (_, res) => res.json({ status: 'ok' }));
 
+const options = {};
 // eslint-disable-next-line init-declarations
-let server;
+let engine;
 if (secure) {
-  const serverOpts = {
-    cert: fs.readFileSync(certPath),
-    key: fs.readFileSync(keyPath),
-  };
-  server = https.createServer(serverOpts, app);
+  try {
+    options.cert = readFileSync(certPath);
+  } catch {
+    console.error(`cert path "${certPath}" invalid`);
+    process.exit(64); // EX_USAGE
+  }
+  try {
+    options.key = readFileSync(keyPath);
+  } catch {
+    console.error(`key path "${keyPath}" invalid`);
+    process.exit(64); // EX_USAGE
+  }
+  engine = https;
 } else {
-  server = http.createServer(app);
+  engine = http;
 }
 
-server.listen(port);
+const server = engine.createServer(options, app);
 
-server.once('listening', () =>
-  // https://googlechrome.github.io/lighthouse-ci/docs/configuration.html#startserverreadypattern
+server.keepAliveTimeout = 5000;
+server.requestTimeout = 10000;
+server.timeout = 15000;
+
+server.listen(port, () =>
   console.log(`Listen local: http${secure ? 's' : ''}://localhost:${port}`),
 );
+
+server.on('timeout', (socket) => {
+  socket.destroy();
+});
 
 server.on('error', (err) => {
   if (err.syscall === 'listen') {
