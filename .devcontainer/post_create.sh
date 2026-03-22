d2f4b67eccdfff64821b6e1e2537045d998791cf
Sebastian Davids <sdavids@gmx.de>
2026-03-22 14:30:23 +0100

    update pnpm to 10.32.1
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,6 +6,6 @@
 find . -type d -name node_modules -exec rm -rf {} +
 
 # pnpm is only used to download and install the NodeJS runtime
-curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION='10.26.2' sh -
+curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION='10.32.1' sh -
 
 bash -i -c 'pnpm env use --global "$(cat .nvmrc)" && npm i --ignore-scripts=true --fund=false --audit=false && npm cache clean --force >/dev/null 2>&1'
